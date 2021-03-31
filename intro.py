92e7e4692e50b254bed9c012ba652793cd94dd49
divakar-lakhera <divakarlakhera99@gmail.com>
2021-03-31 23:13:32 +0530

    update intro.py

--- intro.py
+++ intro.py
@@ -1,4 +1,6 @@
 readme = """
+<b> NIT UK - Grades Manager - Backend API <b><br>
+---------------------------------------------<br><br>
 <b> /session/verify/ </b> <br>
 Accepts :  JSON Object POST Request  { 'user' : 'UserName' , 'pwd' : 'Password Hashed'} <br>
 Returns :  JSON Object {"name": Current User Name, "uid": Unique User ID , "sessKey": SHA256 Session Key, "status": "ok"} <br>
